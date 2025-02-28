69f0ae74b5654b2aeaef3f8419050327b9096599
Shekhar Gupta <shekhar.gupta@ontic.co>
2025-02-28 14:56:37 +0530

    final

--- server.js
+++ server.js
@@ -95,7 +95,7 @@ mongoose
   .catch((err) => console.error("MongoDB connection error:", err));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "/index.html"));
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 app.get("/adhomepage", async (req, res) => {
